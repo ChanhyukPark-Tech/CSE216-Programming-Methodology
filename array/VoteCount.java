92f62539586b39f4a44f075411e784d5ef24a5e2
ChanhyukPark-Tech <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2021-01-27 12:43:50 +0900

    new VoteCount 2021-01-27

--- array/VoteCount.java
+++ array/VoteCount.java
@@ -1,28 +1,25 @@
 import javax.swing.JOptionPane;
-// VoteCount tallies the votes for election candidates.
-// input : a sequence of votes, terminated by a -1
-// output : the listing of the candidates and their tallied votes 
+
 public class VoteCount {
 
 	public static void main(String[] args) {
-		int num_candidates = 4;                       // how many candidates
-		int[] votes = new int[num_candidates]; 	      // holds the votes;
-													  // recall that each element is initialized to 0
-//		collect the votes:
+		int num_candidate = 4;
+		int[] votes = new int[num_candidate];
 		
 		boolean processing = true;
-		while(processing) { 
-			int v = new Integer(JOptionPane.showInputDialog("Vote for (0,1,2,3)")).intValue();
-			
-			if( v >= 0 && v < votes.length) {
-				votes[v] = votes[v] + 1 ;
+		while(processing) {
+			String s = JOptionPane.showInputDialog("type you wanna vote");
+			int wanttovote = new Integer(s).intValue();
+			if(wanttovote >=0 && wanttovote < 4) {
+				votes[wanttovote] ++;
+			} else {
+				processing = false;
 			}
-			else { processing = false;}
-		}
-//		print the totals:
-		
-		for( int i = 0; i != votes.length; i ++ ) {
-			System.out.println("Candidate " + i + " has " + votes[i] + " votes ");
 		}
 		
-	}}
+		for(int i=0 ; i<num_candidate ; i++) {
+			System.out.println("기호" + i + "번 득표수는 " + votes[i] + "  표 입니다."); 
+		}
+	}
+
+}
