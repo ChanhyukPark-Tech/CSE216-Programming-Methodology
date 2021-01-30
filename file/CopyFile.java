7f5a8c463380698c878b683fd6fc4416725311f3
ChanhyukPark-Tech <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2021-01-30 15:08:42 +0900

    new copyfile 2021-01-30

--- file/CopyFile.java
+++ file/CopyFile.java
@@ -1,27 +1,38 @@
-package file;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-import java.io.*;
-
-import javax.swing.*;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class CopyFile {
-
-	public static void main(String[] args) throws IOException {
-		JFileChooser chooser = new JFileChooser();			//JFileChooser 를 사용해서 입력대화상자 만듬
-		chooser.setDialogTitle("choosing file which you wanna copy");
-		int result = chooser.showDialog(null, "COPY");
+	public CopyFile() throws IOException {
+		
+		JFileChooser chooser = new JFileChooser();
+		chooser.setDialogTitle("원하시는 복사파일 " );
+		
+		int result =  chooser.showDialog(null  , "Copy");
 		if(result != chooser.APPROVE_OPTION) {
 			System.exit(0);
+		} else {
+			String s = chooser.getSelectedFile().toString();
+			BufferedReader infile = new BufferedReader(new FileReader(s));
+			PrintWriter outfile = new PrintWriter(new FileWriter(s + ".txt"));
+			while(infile.ready()) {
+				outfile.println(infile.readLine());
+			}
+			
+			infile.close();
+			outfile.close();
 		}
-		String f = chooser.getSelectedFile().toString();
-		BufferedReader infile = new BufferedReader(new FileReader(f));
-		PrintWriter outfile = new PrintWriter(new FileWriter(f + ".out"));
-		while(infile.ready()) {
-			outfile.println(infile.readLine());
-		}
-		infile.close();
-		outfile.close();
 		
+	}
+
+	public static void main(String[] args) throws IOException {
+		new CopyFile();
 		
 	}
 
